0c12266c7c8303a112141eb5f2987d1de89dd569
eownrrnf <128450345+eownrrnf@users.noreply.github.com>
2023-12-08 07:48:04 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 12820 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String b = scanner.nextLine();
+
+        System.out.println(b + "??!");
+
+
+        scanner.close();
+    }
+}
