c9734b9a32d0fac258a82a3600a8b0c295ed2ff2
eownrrnf <128450345+eownrrnf@users.noreply.github.com>
2023-12-16 08:02:32 +0900

    [Bronze V] Title: A+B - 8, Time: 96 ms, Memory: 12236 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        int[][] arr_line = new int[t][2];  
+
+        for(int i = 0; i < t; i++) {
+
+            String[] arr = br.readLine().split(" "); 
+
+            arr_line[i][0] = Integer.parseInt(arr[0]);
+            arr_line[i][1] = Integer.parseInt(arr[1]);
+
+
+        }
+
+        for(int i = 0; i < t; i++) {
+            System.out.print("Case #" + (i + 1) + ": ");
+            System.out.print(arr_line[i][0] + " + " + arr_line[i][1] + " = ");
+            System.out.println(arr_line[i][0] + arr_line[i][1]);
+        }
+
+    }
+}
